f5def916f59d9e80804383aa8d6bc451f9367a9e
vicky113cc <89833235+vicky113cc@users.noreply.github.com>
2025-12-01 16:40:40 +0800

    Update 04_ImageDataGenerator_CNN_formFiles_crop.py

--- Brain-Tumor-MRI/04_ImageDataGenerator_CNN_formFiles_crop.py
+++ Brain-Tumor-MRI/04_ImageDataGenerator_CNN_formFiles_crop.py
@@ -117,17 +117,17 @@ model.add(tf.keras.layers.MaxPooling2D(pool_size=(4, 4)))  # 128x128 -> 32x32
 model.add(tf.keras.layers.Flatten())
 
 # 全連接層 
-model.add(tf.keras.layers.Dense(1000, activation='relu'))
-model.add(tf.keras.layers.BatchNormalization())  # 標準化
-model.add(tf.keras.layers.Dense(100, activation='relu'))
 model.add(tf.keras.layers.Dense(500, activation='relu'))
 model.add(tf.keras.layers.BatchNormalization())  # 標準化
 model.add(tf.keras.layers.Dense(250, activation='relu'))
-model.add(tf.keras.layers.Dense(100, activation='relu'))
+model.add(tf.keras.layers.BatchNormalization())  # 標準化
+model.add(tf.keras.layers.Dense(500, activation='relu'))
+model.add(tf.keras.layers.BatchNormalization())  # 標準化
+model.add(tf.keras.layers.Dense(250, activation='relu'))
 model.add(tf.keras.layers.BatchNormalization())  # 標準化
 model.add(tf.keras.layers.Dense(100, activation='relu'))
 model.add(tf.keras.layers.BatchNormalization())  # 標準化
-model.add(tf.keras.layers.Dense(1000, activation='relu'))
+model.add(tf.keras.layers.Dense(500, activation='relu'))
 model.add(tf.keras.layers.BatchNormalization())  # 標準化
 
 
@@ -143,7 +143,7 @@ model.compile(
 
 # 顯示模型架構
 model.summary()
-learning_rate = 0.01   # 學習率
+learning_rate = 0.1   # 學習率
 opt1 = tf.keras.optimizers.Adam(learning_rate=learning_rate)  # 優化器
 model.compile(
     optimizer=opt1,
